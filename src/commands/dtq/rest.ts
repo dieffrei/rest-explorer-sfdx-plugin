98dd1e42e39e26f7445297b7758a597619eaa6b6
Diéffrei Quadros <dieffrei@gmail.com>
2018-04-21 19:12:13 -0300

    Removed unnecessary flags

--- src/commands/dtq/rest.ts
+++ src/commands/dtq/rest.ts
@@ -28,12 +28,6 @@ export default class Rest extends SfdxCommand {
   // Comment this out if your command does not require an org username
   protected static requiresUsername = true;
 
-  // Comment this out if your command does not support a hub org username
-  protected static supportsDevhubUsername = true;
-
-  // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
-  protected static requiresProject = false;
-
   public async run(): Promise<any> { // tslint:disable-line:no-any
     const name = this.flags.name || 'world';
 
